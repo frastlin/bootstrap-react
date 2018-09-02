95ea17952e16190fd62f76919d264abbfe34756b
Laurence Lewis <laurence.r.lewis@gmail.com>
2018-09-02 10:55:04 +1000

    Added <p></p> around each image
    Add <p></p> around each image for better separation.
    Same as the TABS the images are still not showing. Changes to the path seem to make no difference

--- src/components/Accordion/Alternative/AlternativeRock.js
+++ src/components/Accordion/Alternative/AlternativeRock.js
@@ -3,22 +3,28 @@ import React from "react";
 class AlternativeRock extends React.Component {
   render() {
     return (
-      <div id="accordion-sect1">
+      <div id="accordion-sect1" className="content-panel">
+        <p>
         <img
           src="../../../img/music/ar_et_lts.jpg"
           alt="Emiliana Torrini - Love in the Time of Science"
           title="Emiliana Torrini - Love in the Time of Science"
         />
+        </p>
+        <p>
         <img
           src="../../../img/music/ar_f_ll.jpg"
           alt="Frente - Labour of Love"
           title="Frente - Labour of Love"
         />
+        </p>
+        <p>
         <img
           src="../../../img/music/ar_h_bwpm.jpg"
           alt="Hooverphonic - Blue Wonder Power Milk"
           title="Hooverphonic - Blue Wonder Power Milk"
         />
+        </p>
       </div>
     );
   }
