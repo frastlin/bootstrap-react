58e6546008277a3f5f8d31e2f6e3c32562a483df
Bryan Garaventa <bryan.garaventa@whatsock.com>
2019-01-16 11:44:17 -0800

    Added Bash file for installing dependancies using 'bash ./install.sh' command for quick installation on Mac OS

---
+++ Help/InstallDependenciesBash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+npm install react-html-parser
+npm i bean
+npm install node-fetch --save
+npm install zenscroll
+npm install current-device
---
+++ InstallDependenciesBash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+npm install --global prettier
+npm -g install --global js-beautify
+npm install react-html-parser
+npm i bean
+npm install node-fetch --save
+npm install zenscroll
+npm install current-device
