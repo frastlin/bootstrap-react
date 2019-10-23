8a078ac821a5c46272f4ef56834a6a8d7b4bb9a9
Brandon Biggs <brandonkeithbiggs@gmail.com>
2019-10-23 10:29:25 -0700

    added babel to compile the code to es5.

---
+++ index.js
@@ -0,0 +1,9 @@
+const api = require('./src/AccDC/Core/API')
+
+exports.Accordion = require('./src/react-wrapped-components/Accordion')
+exports.AccordionGroup = require('./src/react-wrapped-components/AccordionGroup')
+exports.Tab = require('./src/react-wrapped-components/Tabs/Tab.js')
+exports.TabList = require('./src/react-wrapped-components/Tabs/TabList.js')
+
+exports.$a = api
+exports.api = api
