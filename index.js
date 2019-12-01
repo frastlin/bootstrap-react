13cb6a25f5849e9e65203f4ba1cf6a2b4cceb6bd
Brandon Biggs <brandonkeithbiggs@gmail.com>
2019-12-01 09:35:15 -0800

    added a build-package command to npm that creates the package in a lib folder

--- index.js
+++
@@ -1,9 +0,0 @@
-const api = require('./src/AccDC/Core/API')
-
-exports.Accordion = require('./src/react-wrapped-components/Accordion')
-exports.AccordionGroup = require('./src/react-wrapped-components/AccordionGroup')
-exports.Tab = require('./src/react-wrapped-components/Tabs/Tab.js')
-exports.TabList = require('./src/react-wrapped-components/Tabs/TabList.js')
-
-exports.$a = api
-exports.api = api
